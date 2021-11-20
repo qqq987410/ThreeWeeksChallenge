a47e96b43f2eaeefe91c25a2602440567a354079
hsuweiwei <qqq987410@gmail.com>
2021-11-20 21:14:16 +0800

    fetch bike API

---
+++ src/data/bike/bikeConstant.js
@@ -0,0 +1,22 @@
+export const bikeVersion = [
+  {
+    type: '1.0',
+  },
+  {
+    type: '2.0',
+  },
+  {
+    type: 'All',
+  },
+];
+
+export const bikeDemand = [
+  {
+    type: '租車',
+    key: 'rent',
+  },
+  {
+    type: '還車',
+    key: 'return',
+  },
+];
